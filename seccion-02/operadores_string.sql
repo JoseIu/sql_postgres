19d8f81b19d64ef83727abfbaffa0f53d4e1cd24
Joselu <joseluissgz97@gmail.com>
2024-07-03 12:36:01 +0200

    refactor: moving all .sql to seccion-02 folder

--- seccion-02/operadores_string.sql
+++ seccion-02/operadores_string.sql
@@ -13,3 +13,7 @@ SELECT
     name,
     SUBSTRING(name, 0,POSITION(' ' IN name)) AS first_name,
     SUBSTRING(name, POSITION(' ' IN name) +1) AS last_name FROM users;
+
+
+UPDATE users SET first_name = SUBSTRING(name, 0, POSITION(' ' IN name)),
+                 last_name = SUBSTRING(name, POSITION(' ' IN name) +1);
