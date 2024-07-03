76de5de7b004080dda54e607032762b520d4f427
Joselu <joseluissgz97@gmail.com>
2024-07-03 12:20:55 +0200

    feat: Add SQL queries for string operations on user names

---
+++ operadores_string.sql
@@ -0,0 +1,15 @@
+SELECT id, UPPER(name) AS name_upper, name FROM users;
+
+SELECT id, LOWER(name) AS name_lower, name FROM users;
+
+SELECT id, LENGTH(name) AS name_length, name FROM users;
+
+
+SELECT name, SUBSTRING(name, 1, 3), POSITION(' ', in name) FROM users;
+
+
+-- +1 para que no tome el espacio
+SELECT
+    name,
+    SUBSTRING(name, 0,POSITION(' ' IN name)) AS first_name,
+    SUBSTRING(name, POSITION(' ' IN name) +1) AS last_name FROM users;
